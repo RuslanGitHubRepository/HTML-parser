92155a228685493eb662ca42dd4df395d57a6452
Ruslan <Ruslan2933@mail.ru>
2019-02-28 13:21:10 +0300

    Удалили класс настройки фильтров

--- src/main/java/com/simbirsoft/kondratyev/ruslan/pizzeria/configuration/SecurityInit.java
+++
@@ -1,6 +0,0 @@
-package com.simbirsoft.kondratyev.ruslan.pizzeria.configuration;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
-}
